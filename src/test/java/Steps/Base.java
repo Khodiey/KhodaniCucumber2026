8a16015b8df8abffd7453c5c6c0ae440acc83a7e
Khodani <krnetshiombo@gmail.com>
2026-05-19 11:14:40 +0200

    added dashboard, createGroup feature

--- src/test/java/Steps/Base.java
+++ src/test/java/Steps/Base.java
@@ -1,4 +1,18 @@
 package Steps;
 
+import Pages.HomePage;
+import Pages.LoginPage;
+import Pages.DashboardPage;
+import Utils.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Base {
+    BrowserFactory browserFactory = new BrowserFactory();
+    final WebDriver driver = browserFactory.startBrowser("chrome",
+            "https://ndosisimplifiedautomation.vercel.app/");
+
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+    LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+    DashboardPage logoutPage = PageFactory.initElements(driver, DashboardPage.class);
 }
